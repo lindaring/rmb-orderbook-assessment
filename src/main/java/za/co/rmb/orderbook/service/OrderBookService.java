d995d280d66f9e51f877c37d243393229c8bc57d
Lindaring <s1m3l4n3@gmail.com>
2023-01-18 15:47:48 +0200

    Updating comments

--- src/main/java/za/co/rmb/orderbook/service/OrderBookService.java
+++ src/main/java/za/co/rmb/orderbook/service/OrderBookService.java
@@ -24,20 +24,22 @@ public class OrderBookService {
       1. Using a Map inorder to group orders per price level (as illustrated in the example limit order book table).
       2. Using TreeMap to maintain ascending/descending order based on price level. Sell side maintains ascending order
          and buy side maintains descending order based on price level.
-      3. Basic operation (containsKey, get, put and remove) at log(n) time cost.
+      3. Basic operation of Treemap (containsKey, get, put and remove) at log(n) time cost.
     */
     Map<Integer, Set<OrderEntity>> sellOrderMap = new TreeMap<>();
     Map<Integer, Set<OrderEntity>> buyOrderMap = new TreeMap<>(Collections.reverseOrder());
 
-    // Using streams for filter BUY / SELL because java can perform both operations in parallel, therefore improving performance
+    // Using streams for looping and to filter BUY/SELL because java can perform both operations in parallel, therefore improving performance
     if (ordersFromDatabase != null) {
       ordersFromDatabase.stream().filter(order -> order != null && order.side() == Side.BUY)
           .forEach(order -> {
             Set<OrderEntity> buyOrderSet = buyOrderMap.get(order.price());
             if (buyOrderSet == null) {
               /*
-                Using TreeSet because we need to maintain ascending order in terms of time. Not using PriorityQueue because
-                It only guarantees min/max value at the top but does not necessarily maintain ascending order.
+                Using TreeSet because we need to maintain ascending order in terms of time (time order was placed).
+                Not using Queue(LinkedList) because it does not maintain order based on a condition. Not using
+                PriorityQueue as well because it only guarantees min or max value at the top of the heap/tree but does
+                not necessarily maintain ascending order for all nodes in the heap/tree based on a condition.
                 TreeSet provides guaranteed log(n) time cost for the basic operations (add, remove and contains).
               */
               buyOrderSet = new TreeSet<>();
@@ -50,7 +52,7 @@ public class OrderBookService {
           .forEach(order -> {
             Set<OrderEntity> sellOrderSet = sellOrderMap.get(order.price());
             if (sellOrderSet == null) {
-              // See line 38-42
+              // See line 38-43
               sellOrderSet = new TreeSet<>();
             }
             sellOrderSet.add(order);
@@ -67,7 +69,7 @@ public class OrderBookService {
       if (side == Side.BUY && this.orderBook.buyOrdersMap() != null) {
         Set<OrderEntity> buyOrderSetEntity = this.orderBook.buyOrdersMap().get(order.price());
         if (buyOrderSetEntity == null) {
-          // See line 38-42
+          // See line 38-43
           buyOrderSetEntity = new TreeSet<>();
         }
         buyOrderSetEntity.add(order);
@@ -76,7 +78,7 @@ public class OrderBookService {
       else if (side == Side.SELL && this.orderBook.sellOrdersMap() != null) {
         Set<OrderEntity> sellOrderSet = this.orderBook.sellOrdersMap().get(order.price());
         if (sellOrderSet == null) {
-          // See line 38-42
+          // See line 38-43
           sellOrderSet = new TreeSet<>();
         }
         sellOrderSet.add(order);
