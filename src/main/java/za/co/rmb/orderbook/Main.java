c7fc8028cd7f25ecf04414d4e65f6dfb38bfcbee
Lindaring <s1m3l4n3@gmail.com>
2023-01-18 14:16:42 +0200

    Adding JUnit and sample test

--- src/main/java/za/co/rmb/orderbook/Main.java
+++ src/main/java/za/co/rmb/orderbook/Main.java
@@ -2,6 +2,5 @@ package za.co.rmb.orderbook;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
   }
 }
---
+++ src/test/java/za/co/rmb/orderbook/service/OrderBookServiceTest.java
@@ -0,0 +1,13 @@
+package za.co.rmb.orderbook.service;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OrderBookServiceTest {
+
+  @Test
+  public void test() {
+    Assert.assertTrue(true);
+  }
+}
